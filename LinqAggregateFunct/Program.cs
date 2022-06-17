fede45ddfeb6ae583456234d9a178459e69b6d47
ciorca <ciorca.gaby@gmail.com>
2022-06-17 19:20:11 +0300

    Add project files.

---
+++ LinqAggregateFunct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqAggregateFunct
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] countries = { "India", "USA", "UK" };
+            Console.WriteLine("Cel mai lung nume de tara are {0} caractere" ,countries.Max(country => country.Length));
+        }
+       
+    }
+}
